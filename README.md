e7a8e53902ba45e5c82ecd29a01b11f4327b3385
b-t-k <boitduvin@hotmail.com>
2020-05-17 10:20:51 -0600

    Update to include Sailboatlistings.com

--- README.md
+++ README.md
@@ -3,7 +3,12 @@ A way to search for boats in the PNW using the Yachtworld website.
 
 ## Why
 As of their last update (late 2019) [YachtWorld.com](https://yachtworld.com), which is the de facto standard for listing boats, you can no longer search for boats in multiple countries at the same time. The PNW ostensibly includes British Columbia, Washington state and Oregon, so you have to perform 2 or 3 separate searches with no way to “save” one region's search and aggregate the results.
-This flask app scrapes the website and returns desired results on a local web page with links back to the original listing.
+It also occurred to me that [sailboatlistings.com](https://sailboatlistings.com) is also a popular venue for selling boats and also has a variety of boats not found on Yachtworld.
+The latest version of this flask app scrapes the Yachtworld website as well as the Sailboatlistings site and returns desired results on a local web page with links back to the original listing.
+
+## What 
+Currently this app searches Yachtworld for BC, Washington and Oregon. Since the Sailboalistings site is set up badly for non U.S. boats, on that it searches for a combination of BC, British Columbia, Vancouver and Washington.
+The results are output to /output/boatlist.json and that file can be saved for other uses by copying it out of the folder.
 
 ## Deployment
 Download and run: $ FLASK_APP=/Search-Yachtworld-flask-app/yacht_app flask run  --port 5000 --host=0.0.0.0
@@ -16,10 +21,15 @@ Obviously python and flask must be installed on your computer for this to run. Y
 ## Variables
 The search is made based on price (minpricenum, maxpricenum), length (lowlen, highlen), currency (currency) and regions. It loops through each region and writes the results to a json file then appends the results from the next iteration of the loop.
 These variables can be adjusted to suit your needs.
-#### line 9 *Set Regions*
-* The base url (line 56) is set to 'https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'. Areas out of North America will need to reset this.
+* Sailboat listing doesn't accept a minimum price variable
+#### *Set Regions* Sailboatlistings
+* The list of city & state variables (line 120) can be modified or deleted.
+* Be sure to also add the variables to the urllist[] array
+ (line 126)
+#### *Set Regions* Yachtworld
+* The base url for Yachtworld (line 62) is set to 'https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'. Areas out of North America will need to reset this.
 * you can add regions using the url parameter from the original site
-* be sure to add them to the list as well (line 104)
+* be sure to add them to the list (line 133) and the variables to the urllist[] array (line 138)
 #### Currency
 * Currency defaults to CAD but additional choices can be added in /templates/index.html
 #### Other variables
--- yacht_app.py
+++ yacht_app.py
@@ -18,6 +18,7 @@ def results():
 def echo():
 	#get index form data
 	if request.method == "POST":
+		sitename=request.form["sitename"]
 		inputcurr=request.form["inputcurr"]
 		minprice=request.form["minprice"]
 		maxprice=request.form["maxprice"]
@@ -53,60 +54,96 @@ def echo():
 			'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
 		}
 
-		# set base url
-		baseurl='https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'
-
-		# input low number
-		if minprice == '':
-			minpricenum = '30000'
-		else:
-			minpricenum = minprice
-		print(minpricenum)
-
-		# input high number
-		if maxprice == '':
-			maxpricenum = '120000'
-		else:
-			maxpricenum = maxprice
-		print(maxpricenum)
+		# set base url &  list site
+		if sitename == "SBL":
+			baseurl='https://www.sailboatlistings.com/cgi-bin/saildata/db.cgi?db=default&uid=default&websearch=1&view_records=1&sb=date&so=descend'
+			listsite= "Sailboatlisting.com"
+		elif sitename == "YW":
+			baseurl='https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'
+			listsite= "Yachtworld.com"
 
 		# input currency
-		if inputcurr == '':
-			curr = 'CAD'
+		curr = inputcurr
+		#print(curr)
+
+		# input low number
+		if sitename == "SBL":
+			minpricenum ='0'
 		else:
-			curr = inputcurr
-		print(curr)
+			if minprice == '':
+				minpricenum = '30000'
+			else:
+				minpricenum = minprice
+		#print(minpricenum)
+
+		# input high number & convert currency
+		exchange = 1.4
+		if sitename == "SBL":
+			if curr == 'CAD':
+				if maxprice == '':
+					maxpricenum = 120000 / exchange
+					maxpricenum = str(math.trunc(maxpricenum))
+				else:
+					maxpricenum = float(maxprice) / exchange
+					maxpricenum = str(math.trunc(maxpricenum))
+			else:
+				if maxprice == '':
+					maxpricenum = '120000'
+				else:
+					maxpricenum = maxprice
+		else:
+			if maxprice == '':
+				maxpricenum = '120000'
+			else:
+				maxpricenum = maxprice
+		#print(maxpricenum)
 
 		# input low length
 		if minlength == '':
 			lowlen = '34'
 		else:
 			lowlen = minlength
-		print(lowlen)
+		#print(lowlen)
 
 		# input high length
 		if maxlength == '':
 			highlen = '48'
 		else:
 			highlen = maxlength
-		print(highlen)
+		#print(highlen)
 
 		# set variables
-		pricerange = '&price=' + minpricenum + '-' + maxpricenum
-		currlen = '?currency=' + curr + '&length=' + lowlen + '-' + highlen
+		if sitename == "SBL":
+			pricerange = '&price-lt=' + maxpricenum
+			boatlength = '&length-gt=' + lowlen + '&length-lt=' + highlen
 
-		# set regions
-		wash = 'country-united-states/state-washington/'
-		oreg = 'country-united-states/state-oregon/'
-		bc = 'country-canada/province-british-columbia/'
+			# create list of url variables
+			bc = '&city=bc'
+			van = '&city=vancouver'
+			british = '&city=british%20columbia'
+			wash = '&state=Washington'
 
-		# create list of region url variables
-		urllist=[bc,wash,oreg]
+			# create list of region url variables
+			urllist=[bc,van,british,wash]
+
+		elif sitename == "YW":
+			pricerange = '&price=' + minpricenum + '-' + maxpricenum
+			currlen = '?currency=' + curr + '&length=' + lowlen + '-' + highlen
+
+			# set regions
+			wash = 'country-united-states/state-washington/'
+			oreg = 'country-united-states/state-oregon/'
+			bc = 'country-canada/province-british-columbia/'
+
+			# create list of region url variables
+			urllist=[bc,wash,oreg]
 
 		# set path to export as file
 		dirname = os.path.dirname(__file__)
 		path_folder = os.path.join(dirname, "output/")
-		print (path_folder)
+		#print (path_folder)
+
+		boatcount = 0
 
 		# set date and time
 		now = datetime.now()
@@ -114,13 +151,20 @@ def echo():
 
 		# create empty list
 		arrayjson = []
-
+		urljson =[]
 		#loop though pages in urllist
 		for page in urllist:
 		# get url
-			urlpath = baseurl+page+currlen+pricerange
-			page = requests.get(urlpath, timeout=5)
+			if sitename == "SBL":
+				urlpath = baseurl+boatlength+pricerange+page
+				#print(urlpath)
+			elif sitename == "YW":
+				urlpath = baseurl+page+currlen+pricerange
 
+			urljson.append(urlpath)
+
+
+			page = requests.get(urlpath, timeout=5)
 			boatpg = BeautifulSoup(page.content, "html.parser")
 
 			# find errors by comparing requested url from listed url
@@ -128,46 +172,185 @@ def echo():
 			if page.url != urlpath:
 				continue
 
-			# find boat listings section
-			boatlist = boatpg.find('div', class_="search-right-col")
+			if sitename == "SBL":
+				# find string with item count in Sailboatlisting
+				pagecountpattern = re.search('(?<=Your search returned )(\d{1,3}) matches.',boatpg.text)
+				#print(pagecountpattern.group(1))
 
-			#find single boat listing
-			boatlisting = boatlist.find_all('a')
+				# set the item count
+				totalitemcount = int(pagecountpattern.group(1))
 
-			#loop though listing and append to list
-			for listname in boatlisting:
-				nameurl = listname['href']
-				thumb = listname.find("meta",  property="image")
-				#add https and find content of meta and substring url to remove first two characters
-				thumburl="https://" + thumb["content"][2:]
-				name = listname.find('div', property="name")
-				priceraw = listname.find('div', class_="price")
-				if priceraw.text != "Call for Price":
-					#remove extra info from front and back
-					price = re.search("\$.*? (?= *)",priceraw.text)
-					cost = price.group()[1:-1]
+				# set page count
+				if totalitemcount > 57:
+					pagecount = math.ceil(totalitemcount/57)
 				else:
-					cost="Call for Price"
-				sizeyear = listname.find('div', class_="listing-card-length-year")
-				location = listname.find('div', class_="listing-card-location")
-				#write to json format
-				writejson =  {
-						"URL": nameurl,
-						"Name": name.text,
-						"Price": cost,
-						"Size": sizeyear.text,
-						"Location":location.text,
-						"Thumb": thumburl
-					}
-				# append to list
-				arrayjson.append(writejson)
+					pagecount = 1
+
+				# set first page
+				pagenum = 1
+
+				while pagecount >= pagenum:
+					#print (pagecount)
+					#print (pagenum)
+
+					currpagehtml = urlpath+'&nh=' + str(pagenum)
+					page = requests.get(currpagehtml, timeout=5)
+					boatpg = BeautifulSoup(page.content, "html.parser")
+					#print (currpagehtml)
+
+#LOOP
+
+					boatlist = boatpg.find_all('table', attrs={'width':'728'})
+
+					#loop though listing and append to list
+					for listname in boatlist:
+						thumbimg = listname.find('img')
+						thumb=thumbimg['src']
+
+						nameurllink=listname.find('a')
+						nameurl = nameurllink['href']
+
+						#add https
+						thumburl="https://www.sailboatlistings.com" + thumb
+						#print(thumburl)
+						name = listname.find('span', class_="sailheader")
+
+						#find postdate
+						postdate=listname.find('span', class_="details")
+						postdate = re.search('\d{2}-[a-zA-Z]{3}-\d{4}',postdate.text)
+						postdate = postdate.group()
+						#print(postdate)
+						#Convert to python date
+						datedate = datetime.strptime(postdate,'%d-%b-%Y').date()
+						#print(datedate)
+
+						#set best before date
+						bestbefore = datetime(2018, 1, 1).date()
+						if datedate < bestbefore:
+							continue
+
+						#find table above td with span that contains details
+						table=listname.find('span', class_="sailvb").parent.parent.parent
+
+						#loop though details table to find details
+						for i,details in enumerate(table.find_all('span')):
+							if i == 1:
+								size = details.text
+							elif i == 7:
+								year = details.text
+							elif i == 15:
+								location = details.text
+							elif i == 17:
+								priceraw = details.text
+
+						#remove $ and comma
+						price = int(priceraw.strip('$').replace(",", ""))
+						if curr == 'CAD':
+							#convert to CAD
+							price = math.trunc(int(price) * exchange)
+						cost = str(price)
+						sizeyear = size + " / " + year
+
+						#write to json format
+						writejson =  {
+								"URL": nameurl,
+								"Name": name.text,
+								"Price": cost,
+								"Size": sizeyear,
+								"Location":location,
+								"Thumb": thumburl,
+								"Listing": listsite,
+								"Listdate": datedate
+							}
+						#increment boat count
+						boatcount = boatcount + 1
+						# append to list
+						arrayjson.append(writejson)
+
+#END LOOP
+
+					#increment pagenumber
+					pagenum = pagenum + 1
+
+			elif sitename == "YW":
+
+				# find string with page count in Yachtworld
+				pagecountstring=boatpg.find('div', class_="page-selector-text")
+				pagecountpattern = re.search('(?<=Viewing )\d{1,2} - (\d{1,2}) of (\d{1,3})',pagecountstring.text)
+
+				# find the two groups that have the item counts
+				curritemcount = int(pagecountpattern.group(1))
+				totalitemcount = int(pagecountpattern.group(2))
+
+				# set page count
+				pagecount = math.ceil(totalitemcount/curritemcount)
+
+				# set first page
+				pagenum = 1
+
+				# Loop through pages
+				while pagecount >= pagenum:
+#					print (pagecount)
+#					print (pagenum)
+
+					currpagehtml = urlpath+'&page=' + str(pagenum)
+					page = requests.get(currpagehtml, timeout=5)
+					boatpg = BeautifulSoup(page.content, "html.parser")
+#					print (currpagehtml)
+
+					# find boat listings section
+					boatlist = boatpg.find('div', class_="search-right-col")
+
+					#find single boat listing
+					boatlisting = boatlist.find_all('a')
+
+					#loop though listing and append to list
+					for listname in boatlisting:
+						nameurl = listname['href']
+						thumb = listname.find("meta",  property="image")
+						#add https and find content of meta and substring url to remove first two characters
+						thumburl="https://" + thumb["content"][2:]
+						name = listname.find('div', property="name")
+						priceraw = listname.find('div', class_="price")
+						if priceraw.text != "Call for Price":
+							#remove extra info from front and back
+							price = re.search("\$.*? (?= *)",priceraw.text)
+							cost = price.group()[1:-1]
+						else:
+							cost="Call for Price"
+						sizeyear = listname.find('div', class_="listing-card-length-year")
+						sizeyear = sizeyear.text
+						location = listname.find('div', class_="listing-card-location")
+						location = location.text
+
+						#write to json format
+						writejson =  {
+								"URL": nameurl,
+								"Name": name.text,
+								"Price": cost,
+								"Size": sizeyear,
+								"Location":location,
+								"Thumb": thumburl,
+								"Listing": listsite
+							}
+						#increment boat count
+						boatcount = boatcount + 1
+
+						# append to list
+						arrayjson.append(writejson)
+
+					#increment pagenumber
+					pagenum = pagenum + 1
 
 		#add Preface list (array)
 		arraypreface = []
 
 		preface = {
 					'Date': dt_string,
-					'Text': 'Results are a Yachtworld search of sailboats in Washington, Oregon and B.C.',
+					'urllisting': urljson,
+					'Text': 'Results are a search of sailboats in Washington, Oregon and B.C. from ',
+					"Listing": listsite,
+					'Boatcount': boatcount,
 					'Currency': curr,
 					'Low': minpricenum,
 					'High': maxpricenum,
