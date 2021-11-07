6c1f21feba94824546eafe523f9038178004f934
BTK <45247934+b-t-k@users.noreply.github.com>
2021-11-07 13:20:03 -0700

    fixed error: name = none

--- flask_boatlisting/search_boatlisting/loops.py
+++ flask_boatlisting/search_boatlisting/loops.py
@@ -10,12 +10,14 @@ from .currencyexchange import cad_conversion, usd_conversion
 # import json
 # import os
 
-#start sailboatlisting_loop
-def sailboatlisting_loop(searchparameters,urljson,arrayjson):
+# start sailboatlisting_loop
+
+
+def sailboatlisting_loop(searchparameters, urljson, arrayjson):
 
     # set variables
     boatcount = 0
-    baseurl='https://www.sailboatlistings.com/cgi-bin/saildata/db.cgi?db=default&uid=default&websearch=1&view_records=1&sb=date&so=descend'
+    baseurl = 'https://www.sailboatlistings.com/cgi-bin/saildata/db.cgi?db=default&uid=default&websearch=1&view_records=1&sb=date&so=descend'
     listsite = 'Sailboatlisting.com'
 
     # Do exchange rate
@@ -25,7 +27,8 @@ def sailboatlisting_loop(searchparameters,urljson,arrayjson):
         maxprice = str(math.trunc(maxprice))
 
     pricerange = '&price-lt=' + maxprice
-    boatlength = '&length-gt=' + searchparameters.minlength + '&length-lt=' + searchparameters.maxlength
+    boatlength = '&length-gt=' + searchparameters.minlength + \
+        '&length-lt=' + searchparameters.maxlength
 
     # create list of url variables
     bc = '&city=bc'
@@ -35,12 +38,11 @@ def sailboatlisting_loop(searchparameters,urljson,arrayjson):
     oreg = '&state=Oregon'
 
     # create list of region url variables
-    urllist=[bc,van,british,wash,oreg]
+    urllist = [bc, van, british, wash, oreg]
 
-
-    #loop though pages in urllist
+    # loop though pages in urllist
     for page in urllist:
-        #set urlpath
+        # set urlpath
         urlpath = baseurl+boatlength+pricerange+page
         print(urlpath)
         urljson.append(urlpath)
@@ -54,8 +56,9 @@ def sailboatlisting_loop(searchparameters,urljson,arrayjson):
             continue
 
         # find string with item count in Sailboatlisting
-        pagecountpattern = re.search('(?<=Your search returned )(\d{1,3}) matches.',boatpg.text)
-        #print(pagecountpattern.group(1))
+        pagecountpattern = re.search(
+            '(?<=Your search returned )(\d{1,3}) matches.', boatpg.text)
+        # print(pagecountpattern.group(1))
 
         # set the item count
         totalitemcount = int(pagecountpattern.group(1))
@@ -69,56 +72,57 @@ def sailboatlisting_loop(searchparameters,urljson,arrayjson):
         # set first page
         pagenum = 1
 
-        #Loop Pages
+        # Loop Pages
         while pagecount >= pagenum:
 
-            #set current page
+            # set current page
             currpagehtml = urlpath+'&nh=' + str(pagenum)
             page = requests.get(currpagehtml, timeout=5)
             boatpg = BeautifulSoup(page.content, "html.parser")
             #print (currpagehtml)
 
-            boatlist = boatpg.find_all('table', attrs={'width':'728'})
+            boatlist = boatpg.find_all('table', attrs={'width': '728'})
 
-            #loop though listing and append to list
+            # loop though listing and append to list
             for listname in boatlist:
                 thumbimg = listname.find('img')
                 if thumbimg is None:
-                    thumb=""
+                    thumb = ""
                     # print("no image")
                 else:
-                    thumb=thumbimg['src']
+                    thumb = thumbimg['src']
                     # print(thumb)
 
-                nameurllink=listname.find('a')
+                nameurllink = listname.find('a')
                 nameurl = nameurllink['href']
 
-                #add https
-                thumburl="https://www.sailboatlistings.com" + thumb
-                #print(thumburl)
+                # add https
+                thumburl = "https://www.sailboatlistings.com" + thumb
+                # print(thumburl)
                 name = listname.find('span', class_="sailheader")
 
-                #find postdate
-                postdate=listname.find('span', class_="details")
-                postdate = re.search('\d{2}-[a-zA-Z]{3}-\d{4}',postdate.text)
+                # find postdate
+                postdate = listname.find('span', class_="details")
+                postdate = re.search('\d{2}-[a-zA-Z]{3}-\d{4}', postdate.text)
                 postdate = postdate.group()
-                #print(postdate)
-                #Convert to python date
-                datedate = datetime.strptime(postdate,'%d-%b-%Y').date()
-                #print(datedate)
+                # print(postdate)
+                # Convert to python date
+                datedate = datetime.strptime(postdate, '%d-%b-%Y').date()
+                # print(datedate)
 
-                #set best before date
+                # set best before date
                 bestbefore = datetime(2018, 1, 1).date()
                 if datedate < bestbefore:
                     continue
 
                 datedate = datedate.strftime('%d-%b-%Y')
 
-                #find table above td with span that contains details
-                table=listname.find('span', class_="sailvb").parent.parent.parent
+                # find table above td with span that contains details
+                table = listname.find(
+                    'span', class_="sailvb").parent.parent.parent
 
-                #loop though details table to find details
-                for i,details in enumerate(table.find_all('span')):
+                # loop though details table to find details
+                for i, details in enumerate(table.find_all('span')):
                     if i == 1:
                         size = details.text
                     elif i == 7:
@@ -128,48 +132,51 @@ def sailboatlisting_loop(searchparameters,urljson,arrayjson):
                     elif i == 17:
                         priceraw = details.text
 
-                #remove $ and comma
+                # remove $ and comma
                 price = int(priceraw.strip('$').replace(",", ""))
                 if searchparameters.inputcurr == 'CAD':
-                    #convert to CAD
+                    # convert to CAD
                     price = math.trunc(int(usd_conversion(price)))
                 cost = str(price)
                 sizeyear = size + " / " + year
 
-                #write to json format
-                writejson =  {
-                        "Name": name.text,
-                        "Price": cost,
-                        "Size": sizeyear,
-                        "Location":location,
-                        "URL": nameurl,
-                        "Thumb": thumburl,
-                        "Listing": listsite,
-                        "Listdate": datedate
-                    }
-                #increment boat count
+                # write to json format
+                writejson = {
+                    "Name": name.text,
+                    "Price": cost,
+                    "Size": sizeyear,
+                    "Location": location,
+                    "URL": nameurl,
+                    "Thumb": thumburl,
+                    "Listing": listsite,
+                    "Listdate": datedate
+                }
+                # increment boat count
                 boatcount = boatcount + 1
 
                 # append to list
                 arrayjson.append(writejson)
 
-            #increment pagenumber
+            # increment pagenumber
             pagenum = pagenum + 1
 
     return listsite, boatcount
 
-#end sailboatlisting_loop
+# end sailboatlisting_loop
 
-#Start yachtworld_loop()
-def yachtworld_loop(searchparameters,urljson,arrayjson):
+# Start yachtworld_loop()
+
+
+def yachtworld_loop(searchparameters, urljson, arrayjson):
 
     # set variables
     boatcount = 0
-    baseurl='https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'
-    listsite= 'Yachtworld.com'
+    baseurl = 'https://www.yachtworld.com/boats-for-sale/type-sail/region-northamerica/'
+    listsite = 'Yachtworld.com'
 
     pricerange = '&price=' + searchparameters.minprice + '-' + searchparameters.maxprice
-    currlen = '?currency=' + searchparameters.inputcurr + '&length=' + searchparameters.minlength + '-' + searchparameters.maxlength
+    currlen = '?currency=' + searchparameters.inputcurr + '&length=' + \
+        searchparameters.minlength + '-' + searchparameters.maxlength
 
     # set regions
     wash = 'country-united-states/state-washington/'
@@ -177,12 +184,12 @@ def yachtworld_loop(searchparameters,urljson,arrayjson):
     bc = 'country-canada/province-british-columbia/'
 
     # create list of region url variables
-    urllist=[bc,wash,oreg]
+    urllist = [bc, wash, oreg]
 
-    #loop though pages in urllist
+    # loop though pages in urllist
     for page in urllist:
 
-        #set urlpath
+        # set urlpath
         urlpath = baseurl+page+currlen+pricerange
         print(urlpath)
         urljson.append(urlpath)
@@ -196,8 +203,9 @@ def yachtworld_loop(searchparameters,urljson,arrayjson):
             continue
 
         # find string with page count in Yachtworld
-        pagecountstring=boatpg.find('div', class_="page-selector-text")
-        pagecountpattern = re.search('(?<=Viewing )\d{1,2} - (\d{1,2}) of (\d{1,3})',pagecountstring.text)
+        pagecountstring = boatpg.find('div', class_="page-selector-text")
+        pagecountpattern = re.search(
+            '(?<=Viewing )\d{1,2} - (\d{1,2}) of (\d{1,3})', pagecountstring.text)
 
         # find the two groups that have the item counts
         curritemcount = int(pagecountpattern.group(1))
@@ -211,8 +219,8 @@ def yachtworld_loop(searchparameters,urljson,arrayjson):
 
         # Loop through pages
         while pagecount >= pagenum:
-                # print (pagecount)
-                # print (pagenum)
+            # print (pagecount)
+            # print (pagenum)
 
             currpagehtml = urlpath+'&page=' + str(pagenum)
             page = requests.get(currpagehtml, timeout=5)
@@ -222,50 +230,56 @@ def yachtworld_loop(searchparameters,urljson,arrayjson):
             # find boat listings section
             boatlist = boatpg.find('div', class_="search-right-col")
 
-            #find single boat listing
+            # find single boat listing
             boatlisting = boatlist.find_all('a')
             # print(boatlisting)
 
-            #loop though listing and append to list
+            # loop though listing and append to list
             for listname in boatlisting:
 
                 if listname['data-reporting-click-listing-type'] != "premium placement":
 
                     nameurl = listname['href']
                     thumb = listname.find("meta",  property="image")
-                    #add https and find content of meta and substring url to remove first two characters
-                    thumburl="https://" + thumb["content"][2:]
-                    name = listname.find('div', property="name")
+                    # add https and find content of meta and substring url to remove first two characters
+                    thumburl = "https://" + thumb["content"][2:]
+                    name = listname.find('h2', property="name")
+                    if not name:
+                        name = "Missing Name"
+                    else:
+                        name = name.text
                     priceraw = listname.find('div', class_="price")
                     if priceraw.text != "Call for Price":
-                        #remove extra info from front and back
-                        price = re.search("\$.*? (?= *)",priceraw.text)
+                        # remove extra info from front and back
+                        price = re.search("\$.*? (?= *)", priceraw.text)
                         cost = price.group()[1:-1]
                     else:
-                        cost="Call for Price"
-                    sizeyear = listname.find('div', class_="listing-card-length-year")
+                        cost = "Call for Price"
+                    sizeyear = listname.find(
+                        'div', class_="listing-card-length-year")
                     sizeyear = sizeyear.text
-                    location = listname.find('div', class_="listing-card-location")
+                    location = listname.find(
+                        'div', class_="listing-card-location")
                     location = location.text
 
-                    #write to json format
-                    writejson =  {
-                            "Name": name.text,
-                            "Price": cost,
-                            "Size": sizeyear,
-                            "Location":location,
-                            "URL": nameurl,
-                            "Thumb": thumburl,
-                            "Listing": listsite,
+                    # write to json format
+                    writejson = {
+                        "Name": name,
+                        "Price": cost,
+                        "Size": sizeyear,
+                        "Location": location,
+                        "URL": nameurl,
+                        "Thumb": thumburl,
+                        "Listing": listsite,
                         "Listdate": ''
-                        }
-                    #increment boat count
+                    }
+                    # increment boat count
                     boatcount = boatcount + 1
 
                     # append to list
                     arrayjson.append(writejson)
 
-            #increment pagenumber
+            # increment pagenumber
             pagenum = pagenum + 1
 
     return listsite, boatcount
